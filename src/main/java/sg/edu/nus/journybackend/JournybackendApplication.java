7a4d52176a97141e79f26832a052f6d4d9ccde0e
Kenneth-Mah <kennethmah@rocketmail.com>
2024-02-24 16:12:50 +0800

    feat: generate project using Spring Initializr

---
+++ src/main/java/sg/edu/nus/journybackend/JournybackendApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.journybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournybackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/journybackend/JournybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.journybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
